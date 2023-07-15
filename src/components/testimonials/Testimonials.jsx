c75d7a0adca6471622c89330fe3058fb4ceae8d3
Emir || 2099 <emirsteveroger@gmail.com>
2023-07-15 20:37:01 +0530

    completed

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,72 @@
 import React from 'react'
 import './testimonials.css'
-function Testimonials() {
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import 'swiper/css/pagination';
+
+
+const data = [
+  {
+      avatar: AVTR1,
+      name: 'Tine Snow',
+      review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus a quidem obcaecati minima mollitia rem nesciunt cupiditate natus provident. Expedita voluptate labore rem natus blanditiis atque ab vitae soluta sequi.Lorem ipsum dolor sit esse unde, pariatur ab libero voluptates placeat.'
+  },
+  {
+      avatar: AVTR2,
+      name: 'Pina Moly',
+      review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus a quidem obcaecati minima mollitia rem nesciunt cupiditate natus provident. Expedita voluptate labore rem natus blanditiis atque ab vitae soluta sequi.Lorem ipsum dolor sit esse unde, pariatur ab libero voluptates placeat.'
+  },
+  {
+      avatar: AVTR3,
+      name: 'Keena Nonny',
+      review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus a quidem obcaecati minima mollitia rem nesciunt cupiditate natus provident. Expedita voluptate labore rem natus blanditiis atque ab vitae soluta sequi.Lorem ipsum dolor sit esse unde, pariatur ab libero voluptates placeat.'
+  },
+  {
+      avatar: AVTR4,
+      name: 'Michael Micky',
+      review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus a quidem obcaecati minima mollitia rem nesciunt cupiditate natus provident. Expedita voluptate labore rem natus blanditiis atque ab vitae soluta sequi.Lorem ipsum dolor sit esse unde, pariatur ab libero voluptates placeat.'
+  },
+]
+
+const Testimonials = () => {
   return (
-    <section id = 'testimonials'>Testimonials</section>
+    <section id = 'testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container"
+       // install Swiper modules
+       modules={[Pagination]}
+       spaceBetween={40}
+       slidesPerView={1}
+       pagination={{ clickable: true }}>
+        {
+          data.map(({avatar, name, review}, index) => {
+              return (
+                <SwiperSlide key={index} className="testimonial">
+                <div className="client__avatar">
+                  <img src={avatar}/>
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className='client__review'>{review}</small>
+                </SwiperSlide>
+              )
+          })
+        }  
+      </Swiper>
+    </section>
   )
 }
 
